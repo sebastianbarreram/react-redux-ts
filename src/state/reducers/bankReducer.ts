c98404631b9ae897295bf5683f781f4e2b1341d4
sebastianbarreram <sbarrera.96@hotmail.com>
2022-09-01 17:37:22 -0500

    Organizing files

--- src/state/reducers/bankReducer.ts
+++ src/state/reducers/bankReducer.ts
@@ -1,31 +1,19 @@
+import { ActionType } from "../actions-types"
+import { Action } from "../actions"
+
 const initialState = 0;
 
-interface DepositAction {
-  type: "deposit";
-  payload: number;
-}
-
-interface WithdrawAction {
-  type: "withdraw";
-  payload: number;
-}
-
-interface BankruptAction {
-  type: "bankrupt";
-}
-type Action = DepositAction | WithdrawAction | BankruptAction;
-
 const reducer = (state: number = initialState, action: Action): number => {
-  switch (action.type) {
-    case "deposit":
-      return state + action.payload;
-    case "withdraw":
-      return state - action.payload;
-    case "bankrupt":
-      return 0;
-    default:
-      return state;
-  }
-};
+    switch (action.type){
+        case ActionType.DEPOSIT:
+            return state + action.payload;
+        case ActionType.WITHDRAW:
+            return state - action.payload;
+        case ActionType.BANKRUPT:
+            return 0;
+        default:
+            return state
+    }
+}
 
-export default reducer;
+export default reducer
